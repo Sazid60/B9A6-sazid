d93a2a1023d8c076551f0967a3395fa946d7f924
Sazid60 <shahnawazsazid60@gmail.com>
2024-03-03 14:41:01 +0600

    Right Card Added & Site Name Changed

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -5,24 +5,24 @@ const latestPostsContainer = document.getElementById('latest-Post-Container')
 const letsDiscussContainer = document.getElementById('lets-discuss-container')
 const leftCardContainer = document.getElementById('left-card-container')
 // Latest post Data Fetch
-const latestPosts = async() =>{
+const latestPosts = async () => {
     const res = await fetch('https://openapi.programming-hero.com/api/retro-forum/latest-posts');
     const data = await res.json();
     // console.log(data)
     displayLatestPost(data)
 }
 // Latest Section Data Display
-const displayLatestPost =(latestPosts) =>{
+const displayLatestPost = (latestPosts) => {
     latestPosts.forEach(latestPost => {
         // console.log(latestPost.cover_image)
         const latestCard = document.createElement('div')
-        latestCard.innerHTML =`
+        latestCard.innerHTML = `
         <div class="card h-[482px] bg-base-100 shadow-xl">
         <figure class="px-5 pt-10">
             <img src="${latestPost.cover_image}" alt="no image"/>
         </figure>
         <div class="px-7 pt-4 items-left text-left mt-0">
-            <p class=""><i class="fa-solid fa-calendar-days"> </i> <span> ${latestPost?.author?.posted_date ? latestPost.author.posted_date :'No Publish Date' }</span></p>
+            <p class=""><i class="fa-solid fa-calendar-days"> </i> <span> ${latestPost?.author?.posted_date ? latestPost.author.posted_date : 'No Publish Date'}</span></p>
             <h2 class="card-title mt-4 text-lg font-bold mb-3">${latestPost?.title}</h2>
             <p class="text-base text-[#12132D63]">${latestPost?.description}</p>
         </div>
@@ -37,7 +37,7 @@ const displayLatestPost =(latestPosts) =>{
 
         </div>
     </div>`
-    latestPostsContainer.appendChild(latestCard)
+        latestPostsContainer.appendChild(latestCard)
     });
 }
 latestPosts()
@@ -45,31 +45,31 @@ latestPosts()
 
 // Lets Discuss Section Data Fetch
 
-const letsDiscussData = async() =>{
+const letsDiscussData = async () => {
     const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts`);
     const data = await res.json();
     const allPosts = data.posts
     displayLetsDiscussData(allPosts)
 }
 // Display All Posts in the Lets Discuss Section 
-const displayLetsDiscussData =(allPosts) =>{
-    
+const displayLetsDiscussData = (allPosts) => {
+
     allPosts.forEach(individualPost => {
         // console.log(individualPost.image)
 
 
-        const latestPostCardLeft = document.createElement('div') 
+        const latestPostCardLeft = document.createElement('div')
         const activeStatus = `${individualPost.isActive}`
         // console.log(activeStatus)
-        let activeStatusLight ='';
-        if(activeStatus === 'true'){
+        let activeStatusLight = '';
+        if (activeStatus === 'true') {
             activeStatusLight = `<span id="active-inactive" class="indicator-item bg-green-400 badge"></span>`
         }
 
-        else{
+        else {
             activeStatusLight = `<span id="active-inactive" class="indicator-item bg-red-600 badge"></span>`
         }
-        latestPostCardLeft.innerHTML =`
+        latestPostCardLeft.innerHTML = `
         <div class="bg-white shadow-xl p-5 lg:p-10 space-y-4 rounded-xl lg:flex lg:gap-6">
         <div class="indicator">
         ${activeStatusLight}
@@ -95,14 +95,41 @@ const displayLetsDiscussData =(allPosts) =>{
                     <p><i class="fa-regular fa-clock"></i><span> ${individualPost.posted_time}</span><span> min</span></p>
                 </div>
                 <div>
-                    <button class="btn rounded-full bg-green-500"><i
+                    <button onclick ="rightDataLoad('${individualPost.title}', '${individualPost.view_count}')"class="btn rounded-full bg-green-500"><i
                             class="fa-regular text-white fa-envelope-open"></i></button>
                 </div>
             </div>
         </div>
     </div>`
-    leftCardContainer.appendChild(latestPostCardLeft)
+        leftCardContainer.appendChild(latestPostCardLeft)
     });
 }
+
+// Loading Data tto Right side card
+let clickCount = 0;
+
+const rightDataLoad = (title, views) => {
+    clickCount++;
+    console.log(clickCount)
+    // console.log(title)
+    // console.log(views)
+    const rightSideCardContainer = document.getElementById('right-card')
+    const rightSideCard = document.createElement('div')
+    rightSideCard.innerHTML = `
+    <!-- Card mini -->
+    <!-- card1-1 -->
+    <div class="space-y-3">
+    <!-- card1-1 -->
+    <div class="shadow-xl bg-white p-4 rounded-xl flex gap-1 items-center">
+        <h1 class="text-xl font-bold mb-4 mt-3 w-[400px]">${title}</h1>
+        <i class="fa-regular fa-eye"> </i><span> ${views}</span>
+    </div>
+    </div>
+    </div>`
+    rightSideCardContainer.appendChild(rightSideCard)
+    const countField = document.getElementById('read-count')
+    countField.innerText = clickCount;
+}
+
 
 letsDiscussData()
